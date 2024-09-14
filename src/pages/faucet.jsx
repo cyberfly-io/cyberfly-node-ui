ec878adb6d5f7fc4c65fb7d5a03daacdbd0f5a01
cyberfly <info@cyberfly.io>
2024-09-14 19:12:18 +0530

    change faucet ui

--- src/pages/faucet.jsx
+++ src/pages/faucet.jsx
@@ -1,30 +1,62 @@
 import { PageContainer } from '@ant-design/pro-components'
-import React, { useEffect, useState } from 'react'
-import { claimFaucet, getNode } from '../services/pact-services'
-import { getNodeInfo } from '../services/node-services'
-import { Button, Result } from 'antd'
-import {WalletOutlined} from "@ant-design/icons"
+import React, { useState } from 'react'
+import { claimCreateFaucet, claimFaucet } from '../services/pact-services'
+import { Button, Result, Segmented, Row, Col, Space } from 'antd'
+import {WalletOutlined, PlusCircleOutlined, MoneyCollectOutlined} from "@ant-design/icons"
 import { useEckoWalletContext } from '../contexts/eckoWalletContext';
 
 const Faucet = () => {
-
   const {initializeEckoWallet, account  } = useEckoWalletContext()
+  const [old, setOld] = useState(false)
 
 
   return (
     <PageContainer title="Testnet Faucet">
+        <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
 
-{ !account? (  <Result
+      <Row>
+      <Col>
+      <Segmented
+      defaultValue="new"
+      onChange={(v)=>{
+        if(v==="old"){
+          setOld(true)
+        }
+        else{
+          setOld(false)
+        }
+      }}
+    options={[
+      { label: 'New Account', value: 'new', icon: <PlusCircleOutlined /> },
+      { label: 'Existing Account', value: 'old', icon: <MoneyCollectOutlined />},
+    ]}
+  />
+      </Col>
+      </Row>
+
+<Row>
+  <Col>
+  { !account? (  <Result
     icon={<WalletOutlined />}
     title="Please connect your kadena wallet to claim testnet CFLY tokens"
     extra={<Button type="primary" onClick={initializeEckoWallet}>Connect</Button>}
   />):(<Button onClick={()=>{
+  if(old){
     claimFaucet(account).then(data=>{
       console.log(data)
     })
+  }
+  else{
+    claimCreateFaucet(account).then(data=>{
+      console.log(data)
+    })
+  }
    }} type='primary'>Claim 50000CFLY</Button>) }
+  </Col>
+</Row>
 
-       
+
+  </Space>
     </PageContainer>
   )
 }
--- src/services/pact-services.js
+++ src/services/pact-services.js
@@ -180,7 +180,7 @@ export const getNode = async (peerId) =>{
   }
 
 
-  export const claimFaucet = async (account)=>{
+  export const claimCreateFaucet = async (account)=>{
   
     const utxn = Pact.builder.execution(`(free.cyberfly_faucet.create-and-request-coin "${account}" (read-keyset 'ks) 50000.0)`)
     .addSigner(getPubkey(account), (withCapability)=>[
@@ -211,6 +211,36 @@ export const getNode = async (peerId) =>{
     }
   }
 
+  export const claimFaucet = async (account)=>{
+  
+    const utxn = Pact.builder.execution(`(free.cyberfly_faucet.request-coin "${account}" 50000.0)`)
+    .addSigner(getPubkey(account), (withCapability)=>[
+      withCapability('free.cyberfly-account-gas-station.GAS_PAYER', 'cyberfly-account-gas', { int: 1 }, 1.0),
+      withCapability('free.cyberfly_node.ALLOW_FUNDING'),
+      withCapability('free.cyberfly_node.BANK_DEBIT'),
+    ])
+    .addData("ks",{pred:"keys-all", keys:[account.split(':')[1]]})
+
+    .setMeta({chainId:"1",senderAccount:"cyberfly-account-gas", gasLimit:2000, gasPrice:0.0000001,ttl: 28000,})
+    .setNetworkId("testnet04")
+    .createTransaction();
+    const  signTransaction = createEckoWalletSign()
+    const signedTx = await signTransaction(utxn)
+    const res = await client.local(signedTx)
+    if(res.result.status==="success"){
+      const txn = await client.submit(signedTx)
+      console.log(txn)
+      pollForTransaction(txn.requestKey, "Claim Testnet Faucet - 50k CFLY", ()=>{console.log("Claim success")})
+      return txn
+    }
+    else{
+      notification.error({
+        message: res.result.error.message,
+        duration: 50000,
+        placement: 'bottomRight',
+      });
+    }
+  }
 
 
 export const pollForTransaction = async (requestKey, message, callback) => {
